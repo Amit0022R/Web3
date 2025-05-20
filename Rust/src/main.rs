4642063defe596c3089da7311a8b58d0b3f3e17f
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2025-05-21 01:35:56 +0530

    Hlo world in RUST

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,5 +1,9 @@
-fn main() {
-    println!("Hello, world!");
-}
+// fn main() {
+//     println!("Hello, world!");
+// }
 // cargo init for install a new cargo project
 // for run it use cargo run
+
+fn main() {
+    println!("Hey, AMiT excited to learn Rust!");
+}
