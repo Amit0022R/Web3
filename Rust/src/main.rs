bccff2286f05a2dde41b36b2eb82d7b63b105ff7
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2025-05-21 19:36:11 +0530

    strings/boolean/variables in rust

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -4,6 +4,49 @@
 // cargo init for install a new cargo project
 // for run it use cargo run
 
+// fn main() {
+//     println!("Hey, AMiT excited to learn Rust!");
+// }
+
+/*  VARIABLES
+    let x: i32 = -5; number can be both positive and negative
+    let y: u32 = 1000; number can be only positive
+    let z: f32 = 1000.001; number can be both positive and negative with decimal
+    println!("x: {}, y: {}, z: {}", x, y, z);
+*/
+
+// fn main() {
+//     let x = 5;
+//     let y = 10;
+//     let sum = x + y;
+//     println!("The sum of {} and {} is {}", x, y, sum);
+// }
+
+
+// BOOLEANS
+
+// fn main() {
+//     let is_male = true;
+//     let is_above_18 = true;
+//     if is_male {
+//         println!("You are a male");
+//     } else {
+//         println!("You are not a male");
+//     }
+//     if is_male && is_above_18 {
+//         print!("You are a legal male");
+//     }
+// }
+
+// STRINGS
+
 fn main() {
-    println!("Hey, AMiT excited to learn Rust!");
+    let greeting = String::from("hello world");
+    println!("{}", greeting);
+    // print!("{}", greeting.chars().nth(1000))
 }
+
+
+
+
+
