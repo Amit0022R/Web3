6085f3b56750f71221183a9eeb392146efdb9313
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2025-06-07 19:31:29 +0530

    add some error logic

--- backendDev/backendProject/src/index.js
+++ backendDev/backendProject/src/index.js
@@ -7,7 +7,22 @@ import dotenv from 'dotenv';
 import connectDB from './db/index.js';
 dotenv.config({path: './.env'});
 
-connectDB();
+connectDB()
+.then( () => {
+
+    app.on("error", (error) => {
+        console.log("error", error);
+        throw error;
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+} )
+.catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1); // Exit the process with failure
+})
 
 
 
