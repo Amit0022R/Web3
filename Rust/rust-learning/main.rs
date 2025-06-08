0190985351c64fc58adf7681beb8023588478a8d
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2025-06-08 17:33:18 +0530

    rust book getting started

---
+++ Rust/rust-learning/main.rs
@@ -0,0 +1,6 @@
+// first compile and run this file using `rustc main.rs &&  .\main.exe`
+// rustfmt main.rs for formatting the code
+fn main() {
+    println!("Hello, world! RUST is awesome!");
+    println!("This is a simple Rust program to demonstrate basic syntax.");
+}
