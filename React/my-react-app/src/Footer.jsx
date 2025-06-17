1343532c0979dd978b9d8c02eae0047646a62633
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2025-06-17 19:45:16 +0530

    basic of react

---
+++ React/my-react-app/src/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <Footer>
+        <p>© 2025 My Website. All rights reserved.</p>
+        <nav>
+            <ul>
+            <li><a href="">Privacy Policy</a></li>
+            <li><a href="">Terms of Service</a></li>
+            <li><a href="">Contact Us</a></li>
+            </ul>
+        </nav>
+    </Footer>
+  )
+}
+
+export default Footer
