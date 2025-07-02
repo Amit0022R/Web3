5d23de201faf7cacea7a95951960b2ada8ca5b55
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2025-07-02 23:55:27 +0530

    add Routing MetaData dynamically

--- nextjs/hello-world/src/app/product/[productId]/page.tsx
+++ nextjs/hello-world/src/app/product/[productId]/page.tsx
@@ -4,17 +4,38 @@
 // nextjs treats folder name in sqaure brackets as a dynamic route
 // e.g. /product/1, /product/2, etc. will be treated as dynamic routes
 
-export default async function ProductDetails({
-    params,
-}: {
-    params: Promise<{ productId: string }>;
-}){
-    const { productId } = await params;
+// export default async function ProductDetails({
+//     params,
+// }: {
+//     params: Promise<{ productId: string }>;
+// }){
+//     const { productId } = await params;
+//     // const productId = (await params).productId;
+//     return (
+//         <h1>Details about product {productId} </h1>
+//     )
+// }
+
+//  Routing MetaData dynamically
+
+import { Metadata } from 'next';
+
+
+type Props = {
+    params: Promise<{productid: string}>;
+} 
+
+export const generateMetadata = async ( { params } : Props) :  Promise<Metadata> => {
+        const id = (await params).productid;
+        return {
+            title: `Product ${id} Details`,
+        };
+}
+
+export default async function ProductDetails({params}: Props){
+    const  productId  = (await params).productid
     // const productId = (await params).productId;
     return (
         <h1>Details about product {productId} </h1>
     )
 }
-
-
-
