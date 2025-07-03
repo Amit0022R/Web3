932a64fafad16dbd7eacbba8147f318bb7a6292b
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2025-07-03 14:46:32 +0530

    add Link concept

--- nextjs/hello-world/src/app/page.tsx
+++ nextjs/hello-world/src/app/page.tsx
@@ -1,5 +1,17 @@
+import Link from "next/link";
+
 export default function Home() {
     return (
-       <h1>Welcome to Home sir!</h1>
-    );
+
+        <>
+       <h1> Home Page sir!</h1>
+       
+       <button>
+       <Link  href="/blog">Blog</Link>
+       </button>
+       <button>
+       <Link  href="/product">Product</Link>
+       </button>
+    </>
+    )
 }
