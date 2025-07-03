64d08ab0226dd8d90c33d4925414ba0d6c424b50
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2025-07-03 14:47:20 +0530

    add link concept

--- nextjs/hello-world/src/app/product/page.tsx
+++ nextjs/hello-world/src/app/product/page.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
 export default function ProductList(){
+    const productId = 4100; // Example product ID, you can change this as needed
     return (
         <div>
+            <button><Link href="/" >Home</Link></button>
            <h1>Product List</h1>
-           <h3>Product 1</h3>
-           <h3>Product 2</h3>
-           <h3>Product 3</h3>
+           <h3><Link href="/product/1" >Product 1</Link></h3>
+           <h3><Link href="/product/2" >Product 2</Link></h3>
+           <h3><Link href="/product/3" >Product 3</Link></h3>
+           <h3><Link href="/product/4" >Product 4</Link></h3>
+           <h3><Link href={`/product/${productId}`} >Product {productId}</Link></h3>
         </div>
     );
 }
